b2adbae0ab2e827e1467b632ad624a8473d1ba46
Mathiasewe <129192031+Mathiasewe@users.noreply.github.com>
2026-06-16 22:32:20 +0200

    add tests

--- gearis/tests/SampleTest.php
+++ gearis/tests/SampleTest.php
@@ -5,4 +5,16 @@ class SampleTest extends TestCase {
     public function testTrueAssertsToTrue() {
         $this->assertTrue(true);
     }
+
+    public function testFrameworkConfigurationIsLoaded() {
+        $conf = core\App::getConf();
+        $this->assertNotNull($conf, "Framework configuration failed to load.");
+    }
+
+    public function testDatabaseConfigurationValues() {
+        $conf = core\App::getConf();
+        $this->assertEquals('gearismysql2026spain.mysql.database.azure.com', $conf->db_server);
+        $this->assertEquals('dbogeris', $conf->db_name);
+    }
+
 }
